59c639d79921bad6388b4d9fad84852a11251c22
Charlin Ignacio <ignaciocharlin1995@gmail.com>
2021-06-11 14:41:33 -0300

    Agregando archivo de bdd

---
+++ tp-final/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Config{
+
+    public static function createDataBase(){
+        include_once "DataBase.php";
+        $config = self::getConfigurationParameters();
+        return new DataBase($config["servername"],$config["usermame"],$config["password"],$config["dbname"]);
+    }
+
+    private static function getConfigurationParameters(){
+        return parse_ini_file("config.ini");
+    }
+
+}
+
+?>
